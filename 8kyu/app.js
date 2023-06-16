16ddab4f77c1f45284e154f05ea15993f9076c40
Ilkhomjon2604 <m347es@gmail.com>
2023-06-16 13:08:20 +0200

    Beginner - Reduce but Grow

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -1,15 +1,26 @@
 // #Kata N1. You get an array of numbers, return the sum of all of the positives ones.
-function positiveSum(arr) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) if(arr[i]>= 0) count += arr[i]
-    // return count;
-    console.log(count);
-}   
-positiveSum([1, 2, 33, 4, 5])
+
+// function positiveSum(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) if(arr[i]>= 0) count += arr[i]
+//     // return count;
+//     console.log(count);
+// }   
+// positiveSum([1, 2, 33, 4, 5])
 
 // #Kata N2. Reversed Strings
 
-function solution(str){
-    return str.split('').reverse().join('')
+// function solution(str){
+//     return str.split('').reverse().join('')
+//   }
+//   console.log(solution('world'));
+
+  // #Kata N3. Beginner - Reduce but Grow
+
+  function grow(x){
+    let sum = 1;
+    for (let i = 0; i < x.length; i++) sum = sum * x[i]
+    console.log(sum);
   }
-  console.log(solution('world'));
+  grow([1, 2, 3])
+
