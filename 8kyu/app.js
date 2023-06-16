bf86775012f3a4c3fdd6a20330cb8a8669019a47
Ilkhomjon2604 <m347es@gmail.com>
2023-06-16 12:40:06 +0200

    positiveSum array is added

---
+++ 8kyu/app.js
@@ -0,0 +1,9 @@
+// #Kata N1. You get an array of numbers, return the sum of all of the positives ones.
+function positiveSum(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) if(arr[i]>= 0) count += arr[i]
+    // return count;
+    console.log(count);
+}   
+positiveSum([1, 2, 33, 4, 5])
+
