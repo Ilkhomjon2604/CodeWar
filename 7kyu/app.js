350c13e2773d89a9d4be12cf1e1e7ff378be6d8d
Ilkhomjon2604 <m347es@gmail.com>
2023-06-17 21:51:43 +0200

    Digit*Digit

---
+++ 7kyu/app.js
@@ -0,0 +1,8 @@
+// #Kata N1. Digit*Digit.
+// let num = 765;
+// let StrNum = num.toString().split('');
+// let newStr = ''
+// for( i = 0 ; i < StrNum.length; i++){
+//     newStr += StrNum[i] * StrNum[i]
+// }
+// console.log(newStr * 1);
