89c12aac4b0591018ee1d691b8fc6f9b92023a52
Ilkhomjon2604 <m347es@gmail.com>
2023-06-17 21:20:15 +0200

    Century From Year

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -48,16 +48,75 @@
 //     console.log(count);
 //   }
 // getCount("abracadabra")
+
+
 // #Kata N7. Invert values
+// function invert(array) {
+//     let newArray = [];
+//     for (i = 0; i < array.length; i++) 
+//       (array[i] >= 0)? newArray.push(array[i] * -1) : newArray.push(Math.abs(array[i]))
+//     console.log(newArray);
+// }
+// invert([1, 2, -3, -4, -5])
 
-function invert(array) {
-    let newArray = [];
-    for (i = 0; i < array.length; i++) 
 
-      (array[i] >= 0)? newArray.push(array[i] * -1) : newArray.push(Math.abs(array[i]))
-  
-    
-    console.log(newArray);
+// #Kata N7. century From Year
 
+function Century(year) {
+    let count = 0;
+    for (let i = 0; i < year; i += 100) count++
+    console.log(count);
 }
-invert([1, 2, -3, -4, -5])
+
+Century(2001)
+
+
+// let cent = 0;
+// if (1<= year && year >= 100){
+//      cent = cent + 1;
+// }else if(101<= year && year >= 200){
+//     cent = cent + 2;
+// }else if(201<= year && year>= 300){
+//     cent = cent + 3;
+// }else if(301<= year && year>= 400){
+//     cent = cent + 4;
+// }else if(400< year && year>= 500){
+//     cent = cent + 5;
+// }else if(500< year && year>= 600){
+//     cent = cent + 6;
+// }else if(600< year && year>= 700){
+//     cent = cent + 7;
+// }else if(700< year && year>= 800){
+//     cent = cent + 8;
+// }
+// else if(800< year && year>= 900){
+//     cent = cent + 9;
+// }else if(900< year && year>= 1000){
+//     cent = cent + 10;
+// }else if(1000< year && year>= 1100){
+//     cent = cent + 11;
+// }else if(1100< year && year>= 1200){
+//     cent = cent + 12;
+// }else if(1200< year && year>= 1300){
+//     cent = cent + 13;
+// }else if(1300< year && year>= 1400){
+//     cent = cent + 14;
+// }else if(1400< year && year>= 1500){
+//     cent = cent + 15;
+// }else if(1500< year && year>= 1600){
+//     cent = cent + 16;
+// }else if(1600< year && year>= 1700){
+//     cent = cent + 17;
+// }else if(1700< year && year>= 1800){
+//    return cent = cent + 18;
+// }else if(1800< year && year>= 1900){
+//     cent = cent + 19;
+// }else if(1900< year && year>= 2000){
+//     cent = cent + 20;
+// }else if(2000< year && year>= 2100){
+//     cent = cent + 21;
+// }
+// else{
+//     console.log('you entered not valid year');
+// }
+// console.log(cent);
