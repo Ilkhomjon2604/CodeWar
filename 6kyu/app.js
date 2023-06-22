ed6d019566423cfeeca85cbc8c01649a1bc4bd3b
Ilkhomjon2604 <m347es@gmail.com>
2023-06-22 19:54:17 +0200

    Stop gninnipS My sdroW!

---
+++ 6kyu/app.js
@@ -0,0 +1,10 @@
+// #Kata N1. Stop gninnipS My sdroW!
+
+function spinWords(string){
+    newStr = string.split(' ');
+    newArr = []
+    for (let i=0; i< newStr.length; i++){
+      (newStr[i].length >= 5) ? newArr.
+    }
+  }
+  console.log(spinWords("Seriously this is the last one"));
