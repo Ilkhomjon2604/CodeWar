37bc71b4b68c0b1809a8f09c4fd22d7985e1d5bb
Ilkhomjon2604 <m347es@gmail.com>
2023-06-23 22:48:27 +0200

    N1. Stop gninnipS My sdroW! first option

--- 6kyu/app.js
+++ 6kyu/app.js
@@ -4,7 +4,8 @@ function spinWords(string){
     newStr = string.split(' ');
     newArr = []
     for (let i=0; i< newStr.length; i++){
-      (newStr[i].length >= 5) ? newArr.
+      (newStr[i].length >= 5) ? newArr.push(newStr[i].split('').reverse().join('')): newArr.push(newStr[i])
     }
+    return newArr.join(' ')
   }
   console.log(spinWords("Seriously this is the last one"));
