c7237b32436813f445537ab7f9891cd0ace64c4d
Ilkhomjon2604 <m347es@gmail.com>
2023-06-23 23:01:58 +0200

    Stop gninnipS My sdroW! final version

--- 6kyu/app.js
+++ 6kyu/app.js
@@ -1,11 +1,14 @@
 // #Kata N1. Stop gninnipS My sdroW!
 
 function spinWords(string){
-    newStr = string.split(' ');
-    newArr = []
-    for (let i=0; i< newStr.length; i++){
-      (newStr[i].length >= 5) ? newArr.push(newStr[i].split('').reverse().join('')): newArr.push(newStr[i])
-    }
-    return newArr.join(' ')
+    word = /\w{5,}/g
+    return string.replace (word, (word) => word.split('').reverse().join('') )
+
+    // newStr = string.split(' ');
+    // newArr = []
+    // for (let i=0; i< newStr.length; i++){
+    //   (newStr[i].length >= 5) ? newArr.push(newStr[i].split('').reverse().join('')): newArr.push(newStr[i])
+    // }
+    // return newArr.join(' ')
   }
   console.log(spinWords("Seriously this is the last one"));
