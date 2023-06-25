7a3b850e83f8c954d7852cc490241fc0afc684ed
Ilkhomjon2604 <m347es@gmail.com>
2023-06-25 23:19:42 +0200

    fix

--- 6kyu/app.js
+++ 6kyu/app.js
@@ -29,10 +29,6 @@
 //       num+= +sum[j]
 //     }
 //    }
-
-
- 
-
 //   return num
 // }
 
@@ -40,37 +36,39 @@
 
 
 
-// #Kata N2. Who likes it?
-function likes(names = 'no one') {
-//  if(!names.length){
-//   return `no one likes this`
-//  }else if(names.length === 1){
-//   return `${names[0]} likes this`
-//  }else if(names.length === 2){
-//   return `${names[0]} and ${names[1]} like this`
-//  }else if(names.length === 3){
-//   return `${names[0]}, ${names[1]} and ${names[2]} like this`
-//  }else {
-//   return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
-//  }
-let x = names.length;
-switch (x) {
-  case 0:
-    return `no one likes this`
-    break;
-  case 1:
-    return `${names[0]} likes this`
-    break;
-    case 2:
-      return `${names[0]} and ${names[1]} like this`
-      break;
-    case 3:
-      return `${names[0]}, ${names[1]} and ${names[2]} like this`
-      break;
-  default:
-    return `${names[0]}, ${names[1]} and ${x - 2} others like this`
-}
-}
+// #Kata N3. Who likes it?
+// function likes(names = 'no one') {
+// //  if(!names.length){
+// //   return `no one likes this`
+// //  }else if(names.length === 1){
+// //   return `${names[0]} likes this`
+// //  }else if(names.length === 2){
+// //   return `${names[0]} and ${names[1]} like this`
+// //  }else if(names.length === 3){
+// //   return `${names[0]}, ${names[1]} and ${names[2]} like this`
+// //  }else {
+// //   return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+// //  }
+// let x = names.length;
+// switch (x) {
+//   case 0:
+//     return `no one likes this`
+//     break;
+//   case 1:
+//     return `${names[0]} likes this`
+//     break;
+//     case 2:
+//       return `${names[0]} and ${names[1]} like this`
+//       break;
+//     case 3:
+//       return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//       break;
+//   default:
+//     return `${names[0]}, ${names[1]} and ${x - 2} others like this`
+// }
+// }
+
+// console.log(likes([ ]));
 
 
-console.log(likes([ ]));
+
