13dbeb395d7845f18a55f38fceb178045c0bf82f
Ilkhomjon2604 <m347es@gmail.com>
2023-07-13 11:02:13 +0200

    // #Kata N7 Multiples of 3 or 5

--- 6kyu/app.js
+++ 6kyu/app.js
@@ -146,5 +146,13 @@
 
 // console.log(duplicateCount("aabBcde"));
 
+// #Kata N7 Multiples of 3 or 5
+
+// function solution(number){
+//     let sum = 0;
+//     for(let i = 0; i < number; i++)  if(i % 3 == 0 || i % 5 == 0)  sum+=i
+//     return sum
+//   }
 
 
+//   console.log(solution(10));
