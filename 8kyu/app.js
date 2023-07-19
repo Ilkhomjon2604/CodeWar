90e5c64a678b5d21fd38f53a815eab26547f3c12
Ilkhomjon2604 <m347es@gmail.com>
2023-07-19 23:31:10 +0200

    training JS #2: Basic data types--Number

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -193,3 +193,42 @@
 //     return x.map(value => value * 2)
 //   }
 //   console.log(maps([1, 2, 3]));
+
+// #Kata18 raining JS #2: Basic data types--Number
+
+// let v1 = 50,
+//     v2 = 100,
+//     v3 = 150,
+//     v4 = 200,
+//     v5 = 2,
+//     v6 = 250;
+
+// function equal1(){
+//   let a = v1,   
+//       b = v1;   
+//   return a + b;
+// }
+
+// //Please refer to the example above to complete the following functions
+// function equal2(){
+//   let a = v4  , //set number value to a
+//       b =  v2; //set number value to b
+//   return a - b;
+// }
+
+// function equal3(){
+//   let a = v1 , //set number value to a
+//       b =  v5; //set number value to b
+//   return a * b;
+// }
+
+// function equal4(){
+//   let a = v4 , //set number value to a
+//       b = v5 ; //set number value to b
+//   return a / b;
+// }
+
+// function equal5(){
+//   let a = v2, //set number value to a
+//       b =  v6; //set number value to b
+//   return a % b;}
